ab8af5412d3b8ee31220dc3ff063f4bc5932560d
Muhammad jamal <jamalkhan2844@gmail.com>
2026-03-01 14:36:26 +0500

    Add files via upload

---
+++ bootstrap_pro1/project_one.js
@@ -0,0 +1,61 @@
+//  create a toggle button that changes the screen to dark-mode when clicked & light-mode when clicked again
+// let modeBtn = document.querySelector("#mode");
+
+// let currMode = "light"; 
+
+// modeBtn.addEventListener("click", ()=>{
+//     if(currMode === "light") {
+//         currMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "black"
+//     }
+//     else{
+//         currMode === "light";
+//         document.querySelector("body").style.backgroundColor = "white"
+//     }
+//     console.log(currMode)
+// })
+
+//  question :create a toggle button that changes the screen to dark-mode when clicked & light-mode when clicked again
+// let modeBtn = document.querySelector("#mode");
+
+// // //              you can also used below for =>   document.querySelector("body")
+// let body = document.querySelector("body");
+// let currMode = "light"; 
+
+// modeBtn.addEventListener("click", () => {
+//     if(currMode === "light") {
+//         currMode = "dark";
+//           body.classList.add("dark")
+//         body.classList.remove("light")
+//     }
+//     else{
+//         currMode === "light";
+//         body.classList.add("light")
+//         body.classList.remove("dark")
+//     }
+//     console.log(currMode)
+// })
+
+
+        //      chatgpt code for toggle
+// let modeBtn = document.querySelector("#mode");
+// let body = document.querySelector("body");
+
+// let currMode = "light";
+
+// modeBtn.addEventListener("click", () => {
+//     if (currMode === "light") {
+//         currMode = "dark";
+//         body.classList.add("dark");
+//         body.classList.remove("light");
+//     } else {
+//         currMode = "light";
+//         body.classList.add("light");
+//         body.classList.remove("dark");
+//     }
+
+//     console.log(currMode);
+// });
+
+
+//              project 2 
